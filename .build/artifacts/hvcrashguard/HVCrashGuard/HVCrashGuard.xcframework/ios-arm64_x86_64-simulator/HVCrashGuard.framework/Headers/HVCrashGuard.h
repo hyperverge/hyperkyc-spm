63e78e1b4b20d1f1bc1f9eacf6df3b31758ec505
Nivedita <mnive93@gmail.com>
2025-12-17 12:34:39 +0530

    updated packages

---
+++ .build/artifacts/hvcrashguard/HVCrashGuard/HVCrashGuard.xcframework/ios-arm64_x86_64-simulator/HVCrashGuard.framework/Headers/HVCrashGuard.h
@@ -0,0 +1,20 @@
+// HVCrashGuard.h
+#import <Foundation/Foundation.h>
+
+// Version number & string
+FOUNDATION_EXPORT double HVCrashGuardVersionNumber;
+FOUNDATION_EXPORT const unsigned char HVCrashGuardVersionString[];
+
+// Public APIs
+#import <HVCrashGuard/HVCrash.h>
+#import <HVCrashGuard/HVCrashConfiguration.h>
+#import <HVCrashGuard/HVCrashInstallation.h>
+#import <HVCrashGuard/HVCrashInstallationStandard.h>
+
+#import <HVCrashGuard/HVCrashReportStore.h>
+#import <HVCrashGuard/HVCrashReport.h>
+#import <HVCrashGuard/HVCrashReportFilter.h>
+#import <HVCrashGuard/HVCrashReportWriter.h>
+#import <HVCrashGuard/HVCrashMonitorType.h>
+
+
