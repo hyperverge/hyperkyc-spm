9e3dcfc1160cbf01232622d66e70fbf15bb395ed
Nivedita <mnive93@gmail.com>
2025-12-19 13:08:28 +0530

    updated packages

--- Package.swift
+++ Package.swift
@@ -11,11 +11,6 @@ let package = Package(
         .library(
             name: "HyperKYC",
             targets: ["HyperKYCWrapper"]
-        ),
-        // Core product: HyperKYC + HyperSnapSDK only (no CrashGuard)
-        .library(
-            name: "HyperKYCCore",
-            targets: ["HyperKYCCoreWrapper"]
         )
     ],
     dependencies: [
@@ -38,12 +33,7 @@ let package = Package(
             checksum: "38cbe9fc82088b218965d8d202f5359f56864ac08e4f6ca06dba9059c32c271f"
         ),
 
-        // Binary target for HyperKYC Core (XCFramework without HVCrashGuard)
-        .binaryTarget(
-            name: "HyperKYCCore",
-            url: "https://hvsdk.s3.ap-south-1.amazonaws.com/ios/release/hyperkyc/1.0.0-beta15/HyperKYCCore-1.0.0-beta15-XCFramework.zip",
-            checksum: "CHECKSUM_FROM_BUILD_SCRIPT_OUTPUT"
-        ),
+       
 
         // Full wrapper: HyperKYC + HyperSnapSDK + CrashGuard
         .target(
@@ -54,19 +44,6 @@ let package = Package(
                 .product(name: "HVCrashGuard", package: "HVCrashGuard")
             ],
             path: "Sources/HyperKYCWrapper",
-            resources: [
-                .copy("Resources")
-            ]
-        ),
-
-        // Core wrapper: HyperKYCCore + HyperSnapSDK only
-        .target(
-            name: "HyperKYCCoreWrapper",
-            dependencies: [
-                "HyperKYCCore",
-                .product(name: "HyperSnapSDK", package: "hypersnapsdk-spm")
-            ],
-            path: "Sources/HyperKYCCoreWrapper",
             resources: [
                 .copy("Resources")
             ]
