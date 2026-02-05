7978e01cd831cade1b199e6087ccaaedf952eda8
Nivedita <mnive93@gmail.com>
2026-02-05 11:32:35 +0530

    added resource bridge

---
+++ Sources/HyperKYCWrapper/ResourceBridge.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+@objcMembers
+public class ResourceBridge: NSObject {
+    /// Returns the SwiftPM-generated resource bundle.
+    @objc public class func spmResourceBundle() -> Bundle {
+        return .module
+    }
+}
