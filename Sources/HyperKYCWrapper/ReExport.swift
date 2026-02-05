6f4dc2c17c069ff645d8ad1ef1cfa56539388bb2
Nivedita <mnive93@gmail.com>
2026-02-05 10:55:22 +0530

    updated to 1.0.0

--- Sources/HyperKYCWrapper/ReExport.swift
+++ Sources/HyperKYCWrapper/ReExport.swift
@@ -1,6 +1,6 @@
 // ReExport.swift
 // Re-exports all dependencies for the HyperKYC product
 // HyperKYCCore is the module name from the XCFramework
-@_exported import HyperKYCCore
+@_exported import HyperKYC
 @_exported import HyperSnapSDK
 @_exported import HVCrashGuard
